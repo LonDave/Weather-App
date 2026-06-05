4e9dcc672b6dee4bc78a655f725802ece8b60b5e
LonDave <space2504@gmail.com>
2026-06-05 21:51:00 +0200

    Aggiunta commenti per migliorare la leggibilità del codice.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.menu');
 const navLinks = document.querySelector('.nav-links');
 
+// Menù burger
 menuButton.addEventListener('click', () =>{
     navLinks.classList.toggle('open');
 });
